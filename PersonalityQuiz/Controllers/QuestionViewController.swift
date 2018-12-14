a3c7fd5d720fd0c8cb3a339a75af25da58eee784
dylanwithgravity <dylanwithgravity@gmail.com>
2018-12-13 22:32:35 -0500

    Created Questions

--- PersonalityQuiz/Controllers/QuestionViewController.swift
+++ PersonalityQuiz/Controllers/QuestionViewController.swift
@@ -9,6 +9,30 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    var questions: [Question] = [
+        Question(text: "How are you feeling today?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Blue", type: .cruise),
+                    Answer(text: "Yellow", type: .roadtrip),
+                    Answer(text: "Green", type: .mountains),
+                    Answer(text: "Black", type: .city)]),
+        Question(text: "Which activities do you want to do?",
+                 type: .multiple,
+                 answers: [
+                   Answer(text: "Swimming", type: .cruise),
+                   Answer(text: "Hiking", type: .mountains),
+                   Answer(text: "Walking", type: .city),
+                   Answer(text: "Driving", type: .roadtrip)]),
+        Question(text: "how much do you like going out of country?",
+                 type: .range,
+                 answers: [
+                    Answer(text: "I dislike", type: .roadtrip),
+                    Answer(text: "I get a little nervous", type: .mountains),
+                    Answer(text: "Maybe a bit", type: .cruise),
+                    Answer(text: "I love other countries!", type: .city)]
+        )]
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- PersonalityQuiz/Models/AnswerData.swift
+++ PersonalityQuiz/Models/AnswerData.swift
@@ -16,4 +16,18 @@ struct Answer {
 
 enum VacationType: Character {
     case city = "🌆", mountains = "🏞", cruise = "🎑", roadtrip = "🌁"
+    
+    var definition: String {
+        switch self {
+        case .city:
+            return "You want to be around a bunch of people. Live life in the city!"
+        case .cruise:
+            return "Venture off to a tropical island, set out to the sea.!"
+        case .mountains:
+            return "Get away from the big building and see some mountains and perhaps go camping!"
+        case .roadtrip:
+            return "Not sure where to go? Hit the Road! Stumble upon new adventures as they come!"
+        }
+    }
+    
 }
