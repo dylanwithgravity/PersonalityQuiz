57fa85fd80f99a8ad79ffed9a0975bd3f6f72261
dylanwithgravity <dylanwithgravity@gmail.com>
2018-12-15 10:26:04 -0500

    Refactored code

--- PersonalityQuiz/Controllers/QuestionViewController.swift
+++ PersonalityQuiz/Controllers/QuestionViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var singlebutton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
+    @IBOutlet weak var multiStackView: UIStackView!
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    
+    var questionIndex = 0
+    
     var questions: [Question] = [
         Question(text: "How are you feeling today?",
                  type: .single,
@@ -36,19 +59,51 @@ class QuestionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
         // Do any additional setup after loading the view.
     }
     
+    func updateUI() {
+        
+        singleStackView.isHidden = true
+        multiStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        let currentQuestion = questions[questionIndex]
+        let currentAnswers = currentQuestion.answers
+        let totalProgress = Float(questionIndex) / Float(questions.count)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        navigationItem.title = "Question #\(questionIndex+1)"
+        questionLabel.text = currentQuestion.text
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStack(using: currentAnswers)
+        case .multiple:
+            updateMultipleStack(using: currentAnswers)
+        case .range:
+            updateRangedStack(using: currentAnswers)
+        }
+        
     }
-    */
+    
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        singlebutton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateMultipleStack(using answers: [Answer]) {
+        multiStackView.isHidden = false
+        multiLabel1.text = answers[0].text
+        multiLabel2.text = answers[1].text
+        multiLabel3.text = answers[2].text
+        multiLabel4.text = answers[4].text
+    }
 
 }
