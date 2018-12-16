01231dd2ef052c5d88cfda8c3ca1bf8e5dd71d51
dylanwithgravity <dylanwithgravity@gmail.com>
2018-12-15 23:23:16 -0500

    User answers are now recorded

--- PersonalityQuiz/Controllers/ResultsViewController.swift
+++ PersonalityQuiz/Controllers/ResultsViewController.swift
@@ -9,22 +9,25 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    
+    var responses: [Answer]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        calculatePersonalityResult()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [VacationType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response] ?? 0) + 1
+        }
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
